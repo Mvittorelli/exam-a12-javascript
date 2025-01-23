5508025c6c4b21b74059117016c7b9ae2ffe4fe7
Mvittorelli <mvittorelli@edenschool.fr>
2025-01-23 10:36:05 +0100

    aaaaaaaaaaaaaaaaaaaaaa it shold work now ig

--- ex08.js
+++ ex08.js
@@ -1,6 +1,6 @@
 function sommePairs(tab) {
 	let sum = 0;
-	if (tab.length >= 1) {
+	if (tab.length >= 1 && !tab.some(isNaN)) {
 		for (i = 0; i < tab.length; i++) {
 			if (tab[i] % 2 === 0) {
 				sum += tab[i];
@@ -11,4 +11,4 @@ function sommePairs(tab) {
 }
 
 console.log(sommePairs([1, 2, 3, 4])); // Résultat attendu : 6
-
+console.log(sommePairs([]));
