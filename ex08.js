f4ca7c9235724b788884dc344505fabe1b0188ff
Mvittorelli <mvittorelli@edenschool.fr>
2025-01-23 10:27:31 +0100

    just trying somthing

--- ex08.js
+++ ex08.js
@@ -1,11 +1,13 @@
 function sommePairs(tab) {
 	let sum = 0;
-	for (i = 0; i < tab.length; i++) {
-		if (tab[i] % 2 === 0) {
-			sum += tab[i];
+	if (tab !== null) {
+		for (i = 0; i < tab.length; i++) {
+			if (tab[i] % 2 === 0) {
+				sum += tab[i];
+			}
 		}
+		return sum;
 	}
-	return sum;
 }
 
 console.log(sommePairs([1, 2, 3, 4])); // Résultat attendu : 6
