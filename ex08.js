c6360cdc78ae3976067257df29a3a634ac2cf320
Mvittorelli <mvittorelli@edenschool.fr>
2025-01-23 10:33:17 +0100

    another try

--- ex08.js
+++ ex08.js
@@ -1,6 +1,6 @@
 function sommePairs(tab) {
 	let sum = 0;
-	if (tab !== null) {
+	if (tab.length >= 1) {
 		for (i = 0; i < tab.length; i++) {
 			if (tab[i] % 2 === 0) {
 				sum += tab[i];
@@ -11,3 +11,4 @@ function sommePairs(tab) {
 }
 
 console.log(sommePairs([1, 2, 3, 4])); // Résultat attendu : 6
+console.log(sommePairs([]));
