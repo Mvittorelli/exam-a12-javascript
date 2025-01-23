6aea256143cd1da96164192623076f21abe89112
Mvittorelli <mvittorelli@edenschool.fr>
2025-01-23 10:25:16 +0100

    maybe?

--- ex07.js
+++ ex07.js
@@ -1,11 +1,13 @@
 function moyenne(tab) {
 	let sum = 0;
 
-	for (let i = 0; i < tab.length; i++) {
-		sum += tab[i];
+	if (tab !== " " || tab !== 0) {
+		for (let i = 0; i < tab.length; i++) {
+			sum += tab[i];
+		}
+		return sum / tab.length;
 	}
-
-	return sum / tab.length;
 }
 
 console.log(moyenne([4, 6, 8])); // Résultat attendu : 6
+console.log(moyenne([0]));
