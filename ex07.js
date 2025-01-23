2b88cb24a4ee4932ca66c5efd985c39221bce39f
Mvittorelli <mvittorelli@edenschool.fr>
2025-01-23 10:17:57 +0100

    it wasnt the final cat

--- ex07.js
+++ ex07.js
@@ -1,13 +1,11 @@
 function moyenne(tab) {
 	let sum = 0;
-	let nb = 0;
 
 	for (let i = 0; i < tab.length; i++) {
 		sum += tab[i];
-		nb++;
 	}
 
-	return sum / nb;
+	return sum / tab.length;
 }
 
 console.log(moyenne([4, 6, 8])); // Résultat attendu : 6
